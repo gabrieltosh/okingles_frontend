b702d03fa3ffcf774a7c5e44a0b5131344c8f7d2
Gabriel Angel <angel22586310@gmail.com>
2020-11-27 22:40:09 -0400

    Image PWA

--- quasar.conf.js
+++ quasar.conf.js
@@ -107,7 +107,7 @@ module.exports = function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#44658e',
         icons: [
           {
             src: 'statics/icons/icon-128x128.png',
