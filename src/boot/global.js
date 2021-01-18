5ef4ef4c92a22dfa407828570d7cea28384b1fce
Gabriel Angel <angel22586310@gmail.com>
2021-01-18 16:24:49 -0400

    Arreglo de servidor

--- src/boot/global.js
+++ src/boot/global.js
@@ -1,6 +1,6 @@
 export default ({ Vue }) => {
   Vue.prototype.$values = {
-    // api: 'https://api.okaprendeingles.com.bo/'
-    api: 'http://localhost:8000/'
+    api: 'https://api.okaprendeingles.com.bo/'
+    //api: 'http://localhost:8000/'
   }
 }
